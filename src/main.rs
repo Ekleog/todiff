481567aba806178df8782ad96530a149f9c653aa
Léo Gaspard <leo@gaspard.io>
2018-03-20 00:51:34 +0100

    Output more pleasant stuff

--- src/main.rs
+++ src/main.rs
@@ -28,19 +28,103 @@ struct TaskChange {
 }
 
 enum Changes {
-    Completed(Option<TaskDate>),
-    Unknown,
+    Finished(bool),
+    Priority(Option<char>),
+    FinishDate(Option<TaskDate>),
+    CreateDate(Option<TaskDate>),
+    Subject(String),
+    DueDate(Option<TaskDate>),
+    ThresholdDate(Option<TaskDate>),
+    Tags((Vec<(String, String)>, Vec<(String, String)>)),
 }
 
-fn changes(from: &Task, to: &Task) -> Changes {
-    if from.subject == to.subject && from.create_date == to.create_date &&
-            from.threshold_date == to.threshold_date && from.due_date == to.due_date &&
-            from.contexts == to.contexts && from.projects == to.projects &&
-            from.hashtags == to.hashtags && from.tags == to.tags && from.finish_date == None &&
-            from.finished == false && to.finished == true {
-        Changes::Completed(to.finish_date)
-    } else {
-        Changes::Unknown
+fn change_str(c: &Changes) -> String {
+    use Changes::*;
+    match *c {
+        Finished(true) => "completed".to_owned(),
+        Finished(false) => "uncompleted".to_owned(),
+        Priority(None) => "priority removed".to_owned(),
+        Priority(Some(c)) => format!("priority set to ({})", c),
+        FinishDate(None) => "completion date removed".to_owned(),
+        FinishDate(Some(d)) => format!("completion date set to {}", d),
+        CreateDate(None) => "creation date removed".to_owned(),
+        CreateDate(Some(d)) => format!("creation date set to {}", d),
+        Subject(ref s) => format!("subject set to ‘{}’", s),
+        DueDate(None) => "due date removed".to_owned(),
+        DueDate(Some(d)) => format!("due date set to {}", d),
+        ThresholdDate(None) => "threshold date removed".to_owned(),
+        ThresholdDate(Some(d)) => format!("threshold date set to {}", d),
+        Tags((ref a, ref b)) => {
+            let mut res = String::new();
+            if a.len() == 1 {
+                res += "removed tag ";
+            } else if a.len() > 1 {
+                res += "removed tags ";
+            }
+            for t in a {
+                res += &format!("{}:{}", t.0, t.1);
+            }
+            if !a.is_empty() && !b.is_empty() {
+                res += " and ";
+            }
+            if b.len() == 1 {
+                res += "added tag ";
+            } else if b.len() > 1 {
+                res += "added tags ";
+            }
+            for t in b {
+                res += &format!("{}:{}", t.0, t.1);
+            }
+            res
+        }
+    }
+}
+
+fn changes(from: &Task, to: &Task) -> Vec<Changes> {
+    use Changes::*;
+
+    let mut res = Vec::new();
+    if from.threshold_date != to.threshold_date {
+        res.push(ThresholdDate(to.threshold_date));
+    }
+    if from.due_date != to.due_date {
+        res.push(DueDate(to.due_date));
+    }
+    if from.finished != to.finished {
+        res.push(Finished(to.finished));
+    }
+    if from.finish_date != to.finish_date {
+        res.push(FinishDate(to.finish_date));
+    }
+    if from.priority != to.priority {
+        if to.priority < 26 {
+            res.push(Priority(Some((b'A' + to.priority) as char)));
+        } else {
+            res.push(Priority(None));
+        }
+    }
+    if from.create_date != to.create_date {
+        res.push(CreateDate(to.create_date));
+    }
+    if from.tags != to.tags {
+        let mut from_t = from.tags.iter().map(|(a, b)| (a.clone(), b.clone())).collect::<Vec<(String, String)>>();
+        let mut to_t = to.tags.iter().map(|(a, b)| (a.clone(), b.clone())).collect::<Vec<(String, String)>>();
+        remove_common(&mut from_t, &mut to_t);
+        res.push(Tags((from_t, to_t)));
+    }
+    if from.subject != to.subject {
+        res.push(Subject(to.subject.clone()));
+    }
+    res
+}
+
+fn remove_common<T: Clone + Eq>(a: &mut Vec<T>, b: &mut Vec<T>) {
+    for x in a.clone().into_iter() {
+        if let Some(b_pos) = b.iter().position(|y| *y == x) {
+            b.remove(b_pos);
+            let a_pos = a.iter().position(|y| *y == x).expect("Internal error E003");
+            a.remove(a_pos);
+        }
     }
 }
 
@@ -60,13 +144,7 @@ fn main() {
     let mut to = read_tasks(matches.value_of("AFTER").expect("Internal error E002"));
 
     // Remove elements in common
-    for x in from.clone().into_iter() {
-        if let Some(pos) = to.iter().position(|y| *y == x) {
-            to.remove(pos);
-            let pos = from.iter().position(|y| *y == x).expect("Internal error E003");
-            from.remove(pos);
-        }
-    }
+    remove_common(&mut from, &mut to);
 
     // Prepare the changeset
     let mut changeset = Vec::new();
@@ -114,13 +192,24 @@ fn main() {
             } else {
                 let num_to = t.to.len();
                 for to in t.to {
-                    use Changes::*;
-                    match changes(&t.orig, &to) {
-                        Completed(Some(d)) => println!("    → Completed at {}", d),
-                        Completed(None) => println!("    → Completed (without date)"),
-                        Unknown if num_to == 1 => println!("    → Changed to ‘{}’", to),
-                        Unknown => println!("    → Copied and changed to ‘{}’", to),
+                    print!("    → ");
+                    let chgs = changes(&t.orig, &to);
+                    for c in 0..chgs.len() {
+                        let chg = change_str(&chgs[c]);
+                        if c == 0 {
+                            let mut chrs = chg.chars();
+                            let first_chr = chrs.next().expect("Internal error E004").to_uppercase();
+                            print!("{}{}", first_chr, chrs.as_str());
+                        } else {
+                            print!("{}", chg);
+                        }
+                        if c < chgs.len() - 2 {
+                            print!(", ");
+                        } else if c == chgs.len() - 2 {
+                            print!(" and ");
+                        }
                     }
+                    println!();
                 }
             }
             println!();
